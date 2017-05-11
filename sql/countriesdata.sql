49a38e9be9d0d53c06c49cd1d7d60fe8cf04a7c6
fulela <fulela.makasi@gmail.com>
2017-05-11 05:55:36 +0200

    Added Installation Instructions and Requirements

---
+++ sql/countriesdata.sql
@@ -0,0 +1,56 @@
+INSERT INTO `vetromedia`.`countries`
+(
+`name`,
+`code`,
+`currency`,
+`currency_code`,
+`surge`,
+`created`,
+`editedby`
+)
+VALUES
+(
+'South Africa',
+'ZA',
+'Rands',
+'ZAR',
+0,
+now(),
+'SQL-User'
+),
+(
+'Great Britain',
+'GB',
+'Pound',
+'GBP',
+5,
+now(),
+'SQL-User'
+),
+(
+'Spain',
+'ESP',
+'Euro',
+'EUR',
+5,
+now(),
+'SQL-User'
+),
+(
+'Kenya',
+'KE',
+'Kenyan Shilling',
+'KES',
+2.5,
+now(),
+'SQL-User'
+),
+(
+'United States of America',
+'USA',
+'US Dollar',
+'USD',
+7.5,
+now(),
+'SQL-User'
+);
